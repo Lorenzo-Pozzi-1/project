71d2034f748dc82746754871d0952c1e3aa0d9fa
Lorenzo <lorenzo.pozzi@wur.nl>
2025-05-12 09:24:43 -0300

    restart with season planner

--- season_planner/__init__.py
+++ season_planner/__init__.py
@@ -1,11 +1,5 @@
 """
 Season Planner package for the LORENZO POZZI Pesticide App.
 
-This package provides components for planning and managing pesticide applications
-across a growing season.
+This package provides components for planning and managing pesticide applications.
 """
-
-from season_planner.season_planner_page import SeasonPlannerPage
-from season_planner.new_season_page import NewSeasonPage
-
-__all__ = ['SeasonPlannerPage', 'NewSeasonPage']
