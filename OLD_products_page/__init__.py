7e6efa4ab04b94eb495ff4aac2a37892808561f7
Lorenzo <lorenzo.pozzi@wur.nl>
2025-05-13 16:00:23 -0300

    Remove old products page and calculator page.

--- OLD_products_page/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Products package for the LORENZO POZZI Pesticide App.
-
-This package provides product listing and comparison functionality.
-"""
-
-from products_page.products_page import ProductsPage
-
-__all__ = ['ProductsPage']
